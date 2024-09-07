8fe98e17c0d6bed20e1ebbaa90ce1c6f40a55877
Ethan Barr <ethanwbarr07@gmail.com>
2024-09-07 09:28:30 +0100

    Added a password length checker

--- fundamentals/password-generator.py
+++ fundamentals/password-generator.py
@@ -8,11 +8,13 @@ characters = list(''.join(raw_characters))
 
 passwordLength = int(input("How many characters fo you want your password: "))
 
+if passwordLength <= 7:
+	print("Enter a number larger then 8")
+	passwordLength = int(input("How many characters fo you want your password: "))
 
-for length in range(passwordLength):
-	selectedCharacter = random.choice(characters)
-
-	password.append(selectedCharacter)
-
-
-print(f"Your password: {''.join(password)}")
+else:
+	for length in range(passwordLength):
+		selectedCharacter = random.choice(characters)
+		password.append(selectedCharacter)
+		
+	print(f"Your password: {''.join(password)}")
